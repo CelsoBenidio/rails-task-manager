f6b8f8c831bf97ba8a93268d4915fab8494f7714
Celso Benidio <celsobenidio@gmail.com>
2020-04-07 15:04:31 +0300

    done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def tasks
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+   def edit
+    #need id for edit
+  end
+
+  def update
+    #need id for update
+    @task.update(tasks_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
+
+end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #see all tasks
+  get 'tasks', to: 'tasks#tasks'
+  post "tasks", to: "tasks#create"
+
+  #create new tasks
+  get "tasks/new", to: "tasks#new"
+
+  #see one specific task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+
+  #edit and update one specfic task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  #delete one specific task
+  delete "tasks/:id", to: "tasks#destroy"
+
+
+
 end
